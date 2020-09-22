02aed61ba0265c7bce43f169ec11e9f34da64c2b
AliAhmed1 <saliahmed11111@gmail.com>
2020-09-22 15:59:38 +0500

    with firebase

--- app.js
+++ app.js
@@ -1,55 +1,76 @@
 var task = document.getElementById("task");
 
+firebase.database().ref('Activity/').on('child_added', function (data) {
 
-function addTask()
-{
-    var inputTodo = document.getElementById("todoValue");
+    // data.forEach(function (childData) {
+    var getValueFromDatabase = data.val().taskname;
+    var setKeyfromDatabase = data.key;
     var addingLi = document.createElement("li")
     addingLi.style.backgroundColor = "yellow"
     // addingLi.style.border = "20px"
     addingLi.style.fontWeight = "bold"
-    var addingText = document.createTextNode(inputTodo.value);
+    // console.log(data.key)
+
+    var addingText = document.createTextNode(getValueFromDatabase);
     addingLi.appendChild(addingText)
     task.appendChild(addingLi);
 
-    inputTodo.value=""
-
 
     var addingBtn = document.createElement("button")
-    var addingBtnText = document.createTextNode("DELETE");
+    var addingBtnText = document.createTextNode("Delete");
     addingBtn.appendChild(addingBtnText)
-    addingBtn.setAttribute("onclick" , "deleteTodo(this)")
-    addingBtn.setAttribute("class" , "deleteTodo")
+    addingBtn.setAttribute("onclick", "deleteTodo(this)")
+    addingBtn.setAttribute("class", "deleteTodo")
     addingLi.appendChild(addingBtn);
 
-
     var addingEdit = document.createElement("button")
-    var addingEditText = document.createTextNode("EDIT");
+    var addingEditText = document.createTextNode("Edit");
     addingEdit.appendChild(addingEditText)
-    addingEdit.setAttribute("onclick" , "editTodo(this)")
-    addingEdit.setAttribute("class" , "editTodo")
+    addingEdit.setAttribute("onclick", "editTodo(this)")
+    addingEdit.setAttribute("class", "editTodo")
     addingLi.appendChild(addingEdit);
+    addingLi.setAttribute("id", setKeyfromDatabase);
+    // })
+})
 
 
 
-    console.log(addingLi);
+
+function addTask() {
+
+    var inputTodo = document.getElementById("todoValue");
+    var key = firebase.database().ref().push().key;
+    var Activity = {
+        taskname: inputTodo.value,
+    }
+    var database = firebase.database().ref('Activity/' + key)
+    database.set(Activity);
+    inputTodo.value = ""
+
 }
 
-function deleteAllTask()
-{
-    task.innerHTML=""
+function deleteAllTask() {
+    task.innerHTML = ""
+    firebase.database().ref('Activity').remove();
+
 }
 
 
-function deleteTodo(e)
-{
+function deleteTodo(e) {
+    // console.log(e.parentNode)
     e.parentNode.remove();
+    const key = e.parentNode.getAttribute("id");
+    firebase.database().ref('Activity/' + key).remove()
+
 }
 
-function editTodo(e)
-{
+function editTodo(e) {
     var editValue = prompt("Edit Value", e.parentNode.firstChild.nodeValue);
     e.parentNode.firstChild.nodeValue = editValue;
-    console.log(e.parentNode.firstChild.nodeValue)
-    
+    const key = e.parentNode.getAttribute("id");
+    console.log(key)
+    firebase.database().ref('Activity/' + key).set({
+        taskname: editValue,
+    });
+
 }
